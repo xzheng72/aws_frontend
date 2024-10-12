c4966d01620f5e4d0d8d23612e66f13306930b75
Xingjie Zheng <JasonZheng192@gmail.com>
2024-10-13 11:45:31 +1300

    fix

--- src/app/api/fetch-ec2metadata/route.ts
+++ src/app/api/fetch-ec2metadata/route.ts
@@ -18,8 +18,14 @@ export async function GET() {
     timeout(3000);
 
     if (response_AZ.ok && response_InstanceId.ok) {
+      console.log('-------');
+      console.log('AZ response:', response_AZ);
+      console.log('Instance Id response:', response_InstanceId);
+      console.log('-------');
+
       const availabilityZone = await response_AZ.text();
       const instanceId = await response_InstanceId.text();
+
       if (availabilityZone && instanceId) {
         return NextResponse.json({ availabilityZone, instanceId });
       } else {
